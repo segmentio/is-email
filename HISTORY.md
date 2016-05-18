8ea8bab0e6d9eaa95727f5e4694462c69612cd37
Nathan Houle <nathan+github@nathanhoule.com>
2016-05-18 12:59:24 -0700

    Modernize test harness; remove Duo support, add Browserify support (#2)

---
+++ HISTORY.md
@@ -0,0 +1,9 @@
+1.0.0 / 2016-05-18
+==================
+
+  * Remove Duo support, add Browserify support
+  * Modernize test harness
+
+0.0.1 - February 11, 2013
+=========================
+:sparkles:
---
+++ test/index.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var isEmail = require('../lib');
+var assert = require('proclaim');
+
+describe('is-email', function() {
+  it('should recognize valid email addresses', function() {
+    assert.strictEqual(isEmail('team@segment.io'), true);
+    assert.strictEqual(isEmail('team+@segmentio.com'), true);
+    assert.strictEqual(isEmail('te-am@segmentio.com'), true);
+    assert.strictEqual(isEmail('team@segmen-tio.com'), true);
+    assert.strictEqual(isEmail('t-eam+34@segme-ntio.com'), true);
+  });
+
+  it('should recognize invalid email addresses', function() {
+    assert.strictEqual(isEmail('team@.org'), false);
+    assert.strictEqual(isEmail('team+45.io'), false);
+    assert.strictEqual(isEmail('@segmentio.com'), false);
+  });
+});
