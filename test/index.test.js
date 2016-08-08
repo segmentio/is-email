7531ebcd01903a9082ec78a541b3f97c7d8d7773
Shawn Inder <shawn@vigour.io>
2016-08-08 13:05:02 -0400

    Add more test cases and make them pass

--- test/index.test.js
+++ test/index.test.js
@@ -10,11 +10,40 @@ describe('is-email', function() {
     assert.strictEqual(isEmail('te-am@segmentio.com'), true);
     assert.strictEqual(isEmail('team@segmen-tio.com'), true);
     assert.strictEqual(isEmail('t-eam+34@segme-ntio.com'), true);
+    assert.strictEqual(isEmail(makeChars(64) + '@segmentio.com'), true); // max length for local-part
   });
 
   it('should recognize invalid email addresses', function() {
     assert.strictEqual(isEmail('team@.org'), false);
     assert.strictEqual(isEmail('team+45.io'), false);
     assert.strictEqual(isEmail('@segmentio.com'), false);
+    assert.strictEqual(isEmail(true), false);
+    assert.strictEqual(isEmail(42), false);
+    assert.strictEqual(isEmail(makeChars), false);
+    assert.strictEqual(isEmail(makeChars(65) + '@segmentio.com'), false); // max length for local-part (too long)
+    assert.strictEqual(isEmail('team@' + makeChars(250) + '.com'), false); // max length for total (too long)
+    assert.strictEqual(isEmail('.team@segmentio.com'), false); // leading `.` in local-part are not allowed
+    assert.strictEqual(isEmail('team.@segmentio.com'), false); // trailing `.` in local-part are not allowed
+    assert.strictEqual(isEmail('te..am@segmentio.com'), false); // 2 consecutive  `.`s in local-part are not allowed
+    assert.strictEqual(isEmail('te am@segmentio.com'), false);
+    assert.strictEqual(isEmail('te(am@segmentio.com'), false);
+    assert.strictEqual(isEmail('te[am@segmentio.com'), false);
+    assert.strictEqual(isEmail('te,am@segmentio.com'), false);
+    assert.strictEqual(isEmail('te:am@segmentio.com'), false);
+    assert.strictEqual(isEmail('te;am@segmentio.com'), false);
+    assert.strictEqual(isEmail('te<am@segmentio.com'), false);
+    assert.strictEqual(isEmail('te@am@segmentio.com'), false);
+    assert.strictEqual(isEmail('te."am"@segmentio.com'), false);
+    assert.strictEqual(isEmail('te"am"@segmentio.com'), false);
+    assert.strictEqual(isEmail('te"am"@segment$io.com'), false);
   });
 });
+
+function makeChars(nb) {
+  var str = '';
+  while (nb > 0) {
+    str += 'a';
+    nb -= 1;
+  }
+  return str;
+}
